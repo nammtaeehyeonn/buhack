fa4898c8e670da789987f6ee705f4abf5eab6d53
NTHHH <ehdro0650@naver.com>
2023-07-08 06:43:09 +0900

    error fix

--- main/urls.py
+++ main/urls.py
@@ -17,6 +17,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+# from django.http import HttpResponse
 
 app_name = 'main'
 urlpatterns = [
--- main/views.py
+++ main/views.py
@@ -10,7 +10,7 @@ import openai
 file_path = '/mnt/c/Users/pc/PycharmProjects/pythonProject7/mysite/main/busan.csv'
 
 # CSV 파일을 데이터프레임으로 변환
-busan_df = pd.read_csv(file_path, encoding='utf8')
+busan_df = pd.read_csv(file_path, encoding='cp949')
 busan_df_data = busan_df[['제목', '위도', '경도', '내용', '이미지주소', '맛집1', '맛집2', '맛집3', '숙소1', '숙소2', '숙소3', '부제', '내용요약']]
 markers = []
 for _, row in busan_df_data.iterrows():
@@ -53,7 +53,7 @@ def question_chat(request):
     if request.method == 'POST':
         question = request.POST.get('question', '')
 
-        openai.api_key = '***'
+        openai.api_key = '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'
 
         messages = []
 
